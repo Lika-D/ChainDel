75e2f67678c2631281bc2685dd0202e73afc61ad
Lika-D <55454606+Lika-D@users.noreply.github.com>
2020-01-06 00:53:03 +0300

    Add files via upload

---
+++ laba5ChainDel/laba5ChainDel/ClassWright.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: student
+ * Дата: 26.11.2019
+ * Время: 19:47
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+
+namespace laba5ChainDel
+{
+	/// <summary>
+	/// Description of ClassWright.
+	/// </summary>
+	public static class ClassWright
+	{
+		public static void WriteLine(string str)
+		{
+			Console.WriteLine(str);
+		}
+		
+		public static void WriteLine(string s1, string s2, int i)
+		{
+			Console.WriteLine(s1, s2, i);
+		}
+	}
+}
